ff85e0840b69ea4db57f959dd8ab251fed74511d
Mikk Martin <mikk.martin@gmail.com>
2021-02-21 21:05:59 +0000

    selector-rect

--- src/SelectorRect.tsx
+++ src/SelectorRect.tsx
@@ -3,10 +3,12 @@ import { motion, useTransform } from 'framer-motion'
 
 export const SelectorRect = () => {
   const {
-    selector: { selecting, startX: x, startY: y, currentX, currentY }
+    selector: { selecting, startX, startY, currentX, currentY }
   } = useContainer()
-  const width = useTransform(currentX, (v) => v - x.get())
-  const height = useTransform(currentY, (v) => v - y.get())
+  const x = useTransform(currentX, (v) => Math.min(v, startX.get()))
+  const y = useTransform(currentY, (v) => Math.min(v, startY.get()))
+  const width = useTransform(currentX, (v) => Math.abs(v - startX.get()))
+  const height = useTransform(currentY, (v) => Math.abs(v - startY.get()))
 
   if (!selecting) return null
   return (
